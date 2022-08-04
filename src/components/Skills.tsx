7adc2182713aa824e3fcf00329f3c54d197d25ab
pdumrauf <dumraufp@gmail.com>
2022-08-04 18:48:14 +0200

    add TS svg / readME

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,6 +6,7 @@ import html from "../assets/img/html.svg";
 import css from "../assets/img/css.svg";
 import js from "../assets/img/js.svg";
 import react from "../assets/img/react.svg";
+import typescript from "../assets/img/typescript.svg";
 import sass from "../assets/img/sass.svg";
 import bootstrap from "../assets/img/bootstrap.svg";
 import git from "../assets/img/git.svg";
@@ -39,20 +40,24 @@ export const Skills = () => {
             <h2>Skills</h2>
             <Carousel responsive={responsive} infinite={true} className="skill-slider">
               <div className="item">
+                <img src={js} alt="ImageJS"/>
+                <h5>JavaScript</h5>
+              </div>              
+              <div className="item">
+                <img src={react} alt="ImageReact"/>
+                <h5>React</h5>
+              </div>
+              <div className="item">
+                <img src={typescript} alt="ImageTypeScript"/>
+                <h5>TypeScript</h5>
+              </div>
+              <div className="item">
                 <img src={html} alt="ImageHTML"/>
                 <h5>HTML</h5>
               </div>
               <div className="item">
                 <img src={css} alt="ImageCSS"/>
                 <h5>CSS</h5>
-              </div>              
-              <div className="item">
-                <img src={js} alt="ImageJS"/>
-                <h5>JavaScript</h5>
-              </div>              
-              <div className="item">
-                <img src={react} alt="ImageReact"/>
-                <h5>ReactJS</h5>
               </div>
               <div className="item">
                 <img src={sass} alt="ImageSass"/>
